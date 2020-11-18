d0424135f040344073e60b241fcd45219bf4d46b
Saya Nishimura <saya.nishimura@i-vinci.co.jp>
2020-11-18 20:20:33 +0900

    local commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h3>My Todo</h3>
+    <input v-model="newTodo" placeholder="Input here...">
+    <button v-on:click="addTodo()">ADD</button>
+
+    <h5>Todo List</h5>
+    <ul>
+      <li v-for="(todo,i) in todos" v-bind:key="i">
+        {{ todo }}
+        <button v-on:click="deleteTodo(i)">DEL</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      todos: [],
+      newTodo: ''
+    }
+  },
+  methods: {
+    addTodo () {
+      if (this.newTodo === '') return;
+      this.todos.push(this.newTodo);
+      this.newTodo = '';
+    },
+    deleteTodo (i) {
+      this.todos.splice(i, 1);
+    }
+  }
+}
+
+</script>
